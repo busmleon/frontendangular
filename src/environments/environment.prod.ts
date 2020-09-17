5057aa339c616c7788325245c07306314b6caa94
busmleon <leon.schoolmail@web.de>
2020-09-17 15:17:22 +0200

    ipv4 with ports

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  KEYCLOAK_URL: 'http://35.242.223.181/auth',
+  KEYCLOAK_URL: 'http://35.242.223.181:8080/auth',
   KEYCLOAK_REALM: 'Test_Realm',
   KEYCLOAK_CLIENT: 'Test_Client',
-  BACKEND_URL: 'http://35.242.223.181/db'
+  BACKEND_URL: 'http://35.242.223.181:3000/db'
 };
