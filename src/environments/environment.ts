5ca1013be960710d793b9109665001092db6dbb1
busmleon <leon.schoolmail@web.de>
2020-09-17 15:58:00 +0200

    localhost with ports

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  KEYCLOAK_URL: 'http://35.242.223.181:8080/auth',
+  KEYCLOAK_URL: 'http://localhost:8080/auth',
   KEYCLOAK_REALM: 'Test_Realm',
   KEYCLOAK_CLIENT: 'Test_Client',
-  BACKEND_URL: 'http://35.242.223.181:3000/db'
+  BACKEND_URL: 'http://localhost:3000/db'
 };
 
 /*
