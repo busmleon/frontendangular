f1dd337144509354568176f811d62097f22fd952
busmleon <leon.schoolmail@web.de>
2020-09-21 12:13:57 +0200

    falsche ip backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   KEYCLOAK_URL: 'https://admin.travellersview.de/auth',
   KEYCLOAK_REALM: 'Test_Realm',
   KEYCLOAK_CLIENT: 'Test_Client',
-  BACKEND_URL: 'https://backend.travellersview.de/db'
+  BACKEND_URL: 'http://35.190.125.211/db'
 };
